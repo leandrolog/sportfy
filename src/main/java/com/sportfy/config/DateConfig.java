22bc76746871a8774d73bc67d0ba300e8fa501f7
leandrolog <leandro.oscargn@gmail.com>
2023-10-29 20:21:13 -0300

    ajuster

--- src/main/java/com/sportfy/config/DateConfig.java
+++ src/main/java/com/sportfy/config/DateConfig.java
@@ -1,6 +1,5 @@
 package com.sportfy.config;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
@@ -13,12 +12,12 @@ import java.time.format.DateTimeFormatter;
 @Configuration
 public class DateConfig {
 
-    public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
 
     @Bean
     @Primary
-    public ObjectMapper objectMapper(){
+    public ObjectMapper objectMapper() {
         JavaTimeModule module = new JavaTimeModule();
         module.addSerializer(LOCAL_DATETIME_SERIALIZER);
         return new ObjectMapper()
